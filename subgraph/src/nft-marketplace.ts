18a27a0313e5d18a893e10ffe45e86ca82507865
meharab <meharablatif@hotmail.com>
2022-10-13 23:12:57 +0600

    configured graph

--- subgraph/src/nft-marketplace.ts
+++ subgraph/src/nft-marketplace.ts
@@ -1,57 +1,103 @@
-import { BigInt } from "@graphprotocol/graph-ts"
 import {
-  NFTMarketplace,
   ListingCanceled,
   ListingCreated,
   ListingPurchased,
-  ListingUpdated
-} from "../generated/NFTMarketplace/NFTMarketplace"
-import { ExampleEntity } from "../generated/schema"
+  ListingUpdated,
+} from "../generated/NFTMarketplace/NFTMarketplace";
+import { store } from "@graphprotocol/graph-ts";
+import { ListingEntity } from "../generated/schema";
 
-export function handleListingCanceled(event: ListingCanceled): void {
-  // Entities can be loaded from the store using a string ID; this ID
-  // needs to be unique across all entities of the same type
-  let entity = ExampleEntity.load(event.transaction.from.toHex())
+export function handleListingCreated(event: ListingCreated): void {
+  // Create a unique ID that refers to this listing
+  // The NFT Contract Address + Token ID + Seller Address can be used to uniquely refer
+  // to a specific listing
+  const id =
+    event.params.nftAddress.toHex() +
+    "-" +
+    event.params.tokenId.toString() +
+    "-" +
+    event.params.seller.toHex();
 
-  // Entities only exist after they have been saved to the store;
-  // `null` checks allow to create entities on demand
-  if (!entity) {
-    entity = new ExampleEntity(event.transaction.from.toHex())
+  // Create a new entity and assign it's ID
+  let listing = new ListingEntity(id);
 
-    // Entity fields can be set using simple assignments
-    entity.count = BigInt.fromI32(0)
-  }
+  // Set the properties of the listing, as defined in the schema,
+  // based on the event
+  listing.seller = event.params.seller;
+  listing.nftAddress = event.params.nftAddress;
+  listing.tokenId = event.params.tokenId;
+  listing.price = event.params.price;
 
-  // BigInt and BigDecimal math are supported
-  entity.count = entity.count + BigInt.fromI32(1)
-
-  // Entity fields can be set based on event parameters
-  entity.nftAddress = event.params.nftAddress
-  entity.tokenId = event.params.tokenId
-
-  // Entities can be written to the store with `.save()`
-  entity.save()
-
-  // Note: If a handler doesn't require existing field values, it is faster
-  // _not_ to load the entity from the store. Instead, create it fresh with
-  // `new Entity(...)`, set the fields that should be updated and save the
-  // entity back to the store. Fields that were not set or unset remain
-  // unchanged, allowing for partial updates to be applied.
-
-  // It is also possible to access smart contracts from mappings. For
-  // example, the contract that has emitted the event can be connected to
-  // with:
-  //
-  // let contract = Contract.bind(event.address)
-  //
-  // The following functions can then be called on this contract to access
-  // state variables and other data:
-  //
-  // - contract.listings(...)
+  // Save the listing to the nodes, so we can query it later
+  listing.save();
 }
 
-export function handleListingCreated(event: ListingCreated): void {}
+export function handleListingCanceled(event: ListingCanceled): void {
+  // Recreate the ID that refers to the listing
+  // Since the listing is being updated, the datastore must already have an entity with this ID
+  // from when the listing was first created
+  const id =
+    event.params.nftAddress.toHex() +
+    "-" +
+    event.params.tokenId.toString() +
+    "-" +
+    event.params.seller.toHex();
 
-export function handleListingPurchased(event: ListingPurchased): void {}
+  // Load the listing to see if it exists
+  let listing = ListingEntity.load(id);
 
-export function handleListingUpdated(event: ListingUpdated): void {}
+  // If it does
+  if (listing) {
+    // Remove it from the store
+    store.remove("ListingEntity", id);
+  }
+}
+
+export function handleListingPurchased(event: ListingPurchased): void {
+  // Recreate the ID that refers to the listing
+  // Since the listing is being updated, the datastore must already have an entity with this ID
+  // from when the listing was first created
+  const id =
+    event.params.nftAddress.toHex() +
+    "-" +
+    event.params.tokenId.toString() +
+    "-" +
+    event.params.seller.toHex();
+
+  // Attempt to load a pre-existing entity, instead of creating a new one
+  let listing = ListingEntity.load(id);
+
+  // If it exists
+  if (listing) {
+    // Set the buyer
+    listing.buyer = event.params.buyer;
+
+    // Save the changes
+    listing.save();
+  }
+}
+
+export function handleListingUpdated(event: ListingUpdated): void {
+  // Recreate the ID that refers to the listing
+  // Since the listing is being updated,
+  // the datastore must already have an entity with this ID
+  // from when the listing was first created
+  const id =
+    event.params.nftAddress.toHex() +
+    "-" +
+    event.params.tokenId.toString() +
+    "-" +
+    event.params.seller.toHex();
+
+  // Attempt to load a pre-existing entity, instead of creating a new one
+  let listing = ListingEntity.load(id);
+
+  // If it exists
+  if (listing) {
+    // Update the price
+    listing.price = event.params.newPrice;
+
+    // Save the changes
+    listing.save();
+  }
+}
